f1a56413906c8d6050f6fe799429fc610255db2f
marcos.test <marcos.test@gmail.com>
2017-06-19 14:42:48 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
   
-  resources :toppages
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+
+  end
+  
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
